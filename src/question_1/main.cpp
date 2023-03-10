61e35a2e066fc6d9cd1b218d7834bbb5ae229931
Joel0192 <123131945+Joel0192@users.noreply.github.com>
2023-03-10 19:51:15 +0000

    q1

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,26 @@
+#include "question1.h"
+#include<iostream>
+using std::cout;
+using std::cin;
 
 int main()
 {
+    auto integer = 0;
+    auto result = 0;
+    do
+    {
+        cout<<"Enter a sales range(or to exit, enter -1): ";
+        cin>>integer;
+        if(integer >= 0)
+        {
+            result = get_sales_commission(integer);
+            cout<<"Sales of "<<integer<<" yields a commission of "<<result<<"\n\n";
+        }
+        
+    } while (integer != -1);
+    
+    
 
+    
     return 0;
 }
--- src/question_1/question1.cpp
+++ src/question_1/question1.cpp
@@ -1,6 +1,37 @@
 #include "question1.h"
+#include<iostream>
+using std::cout;
 
 bool test_config()
 {
+    
     return true;
+}
+
+double get_sales_commission(double sales)
+{
+    auto result = 0;
+
+    if(sales >= 0 && sales <= 499)
+    {
+        result = sales * .05;
+    }
+    else if(sales >= 500 && sales <= 999)
+    {
+        result = sales * .06;
+    }
+    else if(sales >= 1000 && sales <= 1499)
+    {
+        result = sales * .07;
+    }
+    else if(sales >= 1500)
+    {
+        result = sales * .08;
+    }
+    else
+    {
+        result = sales * 0;
+    }
+
+    return result;
 }
