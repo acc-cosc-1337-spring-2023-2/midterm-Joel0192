c62b8bf76e4243844a7ca9140ca88d9bc015b7d3
Joel0192 <123131945+Joel0192@users.noreply.github.com>
2023-03-11 22:31:02 +0000

    Test

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -2,16 +2,66 @@
 #include<iostream>
 using std::cout;
 using std::cin;
+using std::string;
 
 int main()
 {
     int a, b;
+    auto result = 0;
+    auto option = 0;
+    string choice;
 
-    cout<<"Enter two integers."<<"\n";
-    cout<<"#1: ";
-    cin>>a;
-    cout<<"\n"<<"#2: ";
-    cin>>b;
+    do
+    {
+        cout<<"1-Get GCD"<<"\n";
+        cout<<"2-Exit"<<"\n";
+        cout<<"Please enter an option:";
+        cin>> option;
+        
+        if(option == 1)
+        {
+            cout<<"Enter two integers."<<"\n";
+            cout<<"#1: ";
+            cin>>a;
+            cout<<"\n"<<"#2: ";
+            cin>>b;
+            
+            result = find_gcd(a, b);
+
+            if(result >= 1 || result <= 200)
+            {
+                cout<<"The gcd is: "<< result <<"\n";
+            }
+            else
+            {
+                cout<<"Out of range, please try again."<<"\n\n";
+            }
+        }
+        else if(option == 2)
+        {
+            cout<<"are you sure you want to exit?"<<"\n";
+			cout<<"Enter y or n: ";
+			cin>>choice;
+			if(choice == "y"|| choice == "Y")
+			{
+				
+				cout<<"Thank You!"<<"\n";
+				
+			}
+			else
+			{
+				cout<<"\n";
+			}
+        }
+        else
+		{
+			cout<<"Invalid option, Try again"<<"\n\n";
+		}
+    
+
+    }while(choice != "y" && choice != "Y");
+
+    
 
 
     return 0;
--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -7,10 +7,13 @@ bool test_config()
 
 int find_gcd(int num1, int num2)
 {
-    if (num2 == 0)
+    for(int i = num2 <= num1 ? num2:num1; i > 1; i--)
     {
-        return num1;
-        return find_gcd(num2, num1 % num2);
+        if(num1 % i == 0 && num2 % i == 0)
+        {
+            return i;
+        }
+        
     }
 }
     
